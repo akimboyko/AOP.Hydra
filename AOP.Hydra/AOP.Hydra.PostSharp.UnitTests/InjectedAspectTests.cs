55bace0bb19c336c89502728f95fb7d1dd0261ce
Akim Boyko <akim.boyko@gmail.com>
2013-01-08 12:17:26 +0200

    Add PostSharp examples
    Add PostShartp sample application with Injection Aspect example

---
+++ AOP.Hydra/AOP.Hydra.PostSharp.UnitTests/InjectedAspectTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace AOP.Hydra.PostSharp.UnitTests
+{
+    [TestFixture]
+    public class InjectedAspectTests
+    {
+        [Test]
+        public void InjectedAspect_Dependency_ShouldBeCommited()
+        {
+            // Arrange
+            var mockRepository = new MockRepository();
+
+            var transactionMock = mockRepository.DynamicMock<ITransaction>();
+
+            using (mockRepository.Record())
+            {
+                transactionMock.Expect(m => m.Commit()).Repeat.Once();
+            }
+
+            var sut = new FakeService { Transaction = transactionMock };
+
+            // Act
+            sut.Process();
+
+            // Assert
+            transactionMock.VerifyAllExpectations();
+        }
+
+        private class FakeService : IService
+        {
+            public ITransaction Transaction { get; set; }
+
+            [InjectedAspect]
+            public void Process()
+            {
+            }
+        }
+    }
+}
---
+++ AOP.Hydra/AOP.Hydra.PostSharp/InjectionAspect.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using PostSharp.Aspects;
+using PostSharp.Extensibility;
+
+namespace AOP.Hydra.PostSharp
+{
+    public interface ITransaction
+    {
+        void Begin();
+        void Commit();
+        void Rollback();
+    }
+
+    public interface IService
+    {
+        ITransaction Transaction { get; set; }
+    }
+
+    [Serializable]
+    public class InjectedAspectAttribute : MethodInterceptionAspect
+    {
+        public override bool CompileTimeValidate(MethodBase method)
+        {
+            var result = true;
+            var methodInfo = method as MethodInfo;
+
+            if (!typeof(IService).IsAssignableFrom(method.DeclaringType))
+            {
+                var message = string.Format("Only class derived from IService allowed, {0} not implements IService", method.DeclaringType);
+                Message.Write(methodInfo, SeverityType.Error, "999", message);
+
+                result = false;
+            }
+
+            return result;
+        }
+
+        public override void OnInvoke(MethodInterceptionArgs args)
+        {
+            args.Proceed();
+
+            if (args.Instance is IService)
+            {
+                (args.Instance as IService).Transaction.Commit();
+            }
+        }
+    }
+}
