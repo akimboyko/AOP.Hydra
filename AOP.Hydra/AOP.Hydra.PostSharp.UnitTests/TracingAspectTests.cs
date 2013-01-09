91e2cf738b9a182050b0473d16c960a47e04757b
Akim Boyko <akim.boyko@gmail.com>
2013-01-09 19:20:18 +0200

    Add OnMethodBoundaryAspect example

---
+++ AOP.Hydra/AOP.Hydra.PostSharp.UnitTests/TracingAspectTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOP.Hydra.PostSharp.UnitTests
+{
+    [TestFixture]
+    public class TracingAspectTests
+    {
+        [Test]
+        public void TracingAspect_OnEntry_AddInfo()
+        {
+            var result = new FakeService().Process(3, 4);
+
+            Assert.That(result, Is.EqualTo(12));
+
+            FakeLoggerFactory.FakeLoggerInstance.Messages.Should().Contain(message => 
+                                                                            message.Contains("OnEntry")
+                                                                            && message.Contains(string.Format("{0}.Process(...)", typeof(FakeService).FullName)));
+
+            FakeLoggerFactory.FakeLoggerInstance.Messages.Should().Contain(message => 
+                                                                            message.Contains("OnExit")
+                                                                            && message.Contains(string.Format("{0}.Process(...)", typeof(FakeService).FullName)));
+
+            FakeLoggerFactory.FakeLoggerInstance.Messages.Should().Contain(message => 
+                                                                            message.Contains("OnSuccess")
+                                                                            && message.Contains(string.Format("{0}.Process(...)", typeof(FakeService).FullName)));
+        }
+
+        [Test]
+        public void TracingAspect_OnException_AddInfo()
+        {
+            var fakeService = new FakeService();
+
+            Assert.Throws<Exception>(() => fakeService.ProcessWithException(3, 4));
+
+            FakeLoggerFactory.FakeLoggerInstance.Messages.Should().Contain(message =>
+                                                                            message.Contains("OnEntry")
+                                                                            && message.Contains(string.Format("{0}.ProcessWithException(...)", typeof(FakeService).FullName)));
+
+            FakeLoggerFactory.FakeLoggerInstance.Messages.Should().Contain(message =>
+                                                                            message.Contains("OnExit")
+                                                                            && message.Contains(string.Format("{0}.ProcessWithException(...)", typeof(FakeService).FullName)));
+
+            FakeLoggerFactory.FakeLoggerInstance.Messages.Should().Contain(message =>
+                                                                            message.Contains("OnException")
+                                                                            && message.Contains(string.Format("{0}.ProcessWithException(...)", typeof(FakeService).FullName)));
+        }
+
+        [TracingAspect(AbstractFactoryType = typeof(FakeLoggerFactory))]
+        private class FakeService
+        {
+            public int Process(int n, int m)
+            {
+                return n * m;
+            }
+
+            public int ProcessWithException(int n, int m)
+            {
+                throw new Exception(string.Format("An exception occures while calculating {0} * {1}", n, m));
+            }
+        }
+
+        private class FakeLogger : ILogger
+        {
+            private readonly IList<string> _messages = new List<string>();
+
+            public IReadOnlyCollection<string> Messages
+            {
+                get
+                {
+                    return new ReadOnlyCollection<string>(_messages);
+                }
+            }
+
+            public void Log(string message)
+            {
+                _messages.Add(message);
+            }
+        }
+
+        private class FakeLoggerFactory : IAbstractFactory<FakeLogger>
+        {
+            public static readonly FakeLogger FakeLoggerInstance = new FakeLogger();
+
+            public FakeLogger CreateInstance()
+            {
+                return FakeLoggerInstance;
+            }
+        }
+    }
+}
---
+++ AOP.Hydra/AOP.Hydra.PostSharp/TracingAspectAttribute.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Reflection;
+using PostSharp.Aspects;
+using PostSharp.Aspects.Dependencies;
+using PostSharp.Extensibility;
+
+namespace AOP.Hydra.PostSharp
+{
+    [Serializable]
+    [ProvideAspectRole(StandardRoles.Tracing)]
+    [AspectRoleDependency(AspectDependencyAction.Order, AspectDependencyPosition.Before, StandardRoles.Validation)]
+    public class TracingAspectAttribute : OnMethodBoundaryAspect
+    {
+        public Type AbstractFactoryType { get; set; }
+        private ILogger logger { get; set; }
+
+        /// <summary>
+        /// Compiles the time validate.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <returns>Is validation passed</returns>
+        public override bool CompileTimeValidate(MethodBase method)
+        {
+            var result = true;
+
+            var methodInfo = method as MethodInfo;
+
+            if (AbstractFactoryType == null) // check that AbstractFactoryType is provided 
+            {
+                var message = string.Format("AbstractFactoryType is null. For aspect {0} applied to {1}", GetType().Name, method.DeclaringType);
+                Message.Write(methodInfo, SeverityType.Error, "999", message);
+
+                result = false;
+            }
+            else if (!(typeof(IAbstractFactory<ILogger>).IsAssignableFrom(AbstractFactoryType))) // check that AbstractFactoryType is proper type
+            {
+                var message = string.Format("Only abstract facory derived from IAbstractFactory<ILogger> allowed, {0} not implements IAbstractFactory<ILogger>. For aspect {1} applied to {2}", 
+                                                AbstractFactoryType, GetType().Name, method.DeclaringType);
+
+                Message.Write(methodInfo, SeverityType.Error, "999", message);
+
+                result = false;
+            }
+            else if (AbstractFactoryType.IsAbstract || AbstractFactoryType.IsInterface) // check that instance of AbstractFactoryType could be created
+            {
+                var message = string.Format("Only concrete facory derived from IAbstractFactory<ILogger> allowed, {0} not implements IAbstractFactory<ILogger>", AbstractFactoryType);
+                Message.Write(methodInfo, SeverityType.Error, "999", message);
+
+                result = false;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Initializes the current aspect.
+        /// </summary>
+        /// <param name="method">Method to which the current aspect is applied.</param>
+        public override void RuntimeInitialize(MethodBase method)
+        {
+            // create instance of Abstract Factory
+            var abstractFactory = Activator.CreateInstance(AbstractFactoryType);
+
+            // create instance of dependency
+            logger = ((IAbstractFactory<ILogger>)abstractFactory).CreateInstance();
+        }
+
+        /// <summary>
+        /// Method executed <b>before</b> the body of methods to which this aspect is applied.
+        /// </summary>
+        /// <param name="args">Event arguments specifying which method
+        /// is being executed, which are its arguments, and how should the execution continue
+        /// after the execution of <see cref="M:PostSharp.Aspects.IOnMethodBoundaryAspect.OnEntry(PostSharp.Aspects.MethodExecutionArgs)" />.</param>
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            logger.Log(string.Format("OnEntry {0}.{1}(...)", args.Method.DeclaringType.FullName, args.Method.Name));
+        }
+
+        /// <summary>
+        /// Method executed <b>after</b> the body of methods to which this aspect is applied,
+        /// even when the method exists with an exception (this method is invoked from
+        /// the <c>finally</c> block).
+        /// </summary>
+        /// <param name="args">Event arguments specifying which method
+        /// is being executed and which are its arguments.</param>
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            logger.Log(string.Format("OnExit {0}.{1}(...)", args.Method.DeclaringType.FullName, args.Method.Name));
+        }
+
+        /// <summary>
+        /// Method executed <b>after</b> the body of methods to which this aspect is applied,
+        /// but only when the method successfully returns (i.e. when no exception flies out
+        /// the method.).
+        /// </summary>
+        /// <param name="args">Event arguments specifying which method
+        /// is being executed and which are its arguments.</param>
+        public override void OnSuccess(MethodExecutionArgs args)
+        {
+            logger.Log(string.Format("OnSuccess {0}.{1}(...)", args.Method.DeclaringType.FullName, args.Method.Name));
+        }
+
+        /// <summary>
+        /// Method executed <b>after</b> the body of methods to which this aspect is applied,
+        /// in case that the method resulted with an exception.
+        /// </summary>
+        /// <param name="args">Event arguments specifying which method
+        /// is being executed and which are its arguments.</param>
+        public override void OnException(MethodExecutionArgs args)
+        {
+            logger.Log(string.Format("OnException {0}.{1}(...) Exception: {2}", args.Method.DeclaringType.FullName, args.Method.Name, args.Exception));
+        }
+    }
+}
